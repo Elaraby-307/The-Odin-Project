990a5b825c862b38363d3e297cd8d609ec3945d9
Mohamed Elaraby <mohamedelarabi307@gmail.com>
2026-07-08 17:32:02 +0300

    project modified to implement the exact requirements

--- first_path_library/script.js
+++ first_path_library/script.js
@@ -1,150 +1,156 @@
-// invoccation variables
-const div = document.getElementById('div');
-const btn = document.getElementById('btn');
-const send = document.querySelector('.send');
-const dialog = document.getElementById('dialog');
-const inp = document.getElementsByClassName('inp')
-const form = document.getElementById('form')
+let div = document.getElementById('div')
+let dialog = document.getElementById('dialog')
+let new_btn = document.getElementById('btn')
+let form = document.getElementById('form')
+
+let library = []
 
 
-
-
-btn.setAttribute('style', 'background-color: lightgreen; color: white; margin: 15px; font-weight: bold; font-size: 20px;  cursor: pointer;')
-
-// variable declaration
-
-
-
-
-const library = []
-
-function books(id, title, author, pages, read) {
-
+function books (id ,title, author, pages, read){
     if(!new.target){
-        throw Error('You must use the new keyword to create a new book')
+        throw Error("Please use constructor with new")
     }
-
     this.id = id
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-
 }
 
-books.prototype.read = function(){
-    (this.read === true) ? this.read = false : this.read = true
-
-}
-
-
-
-function addBookToLibrary(title, author, pages, read) {
-
-    let book = new books(crypto.randomUUID(), title, author, pages, read);
-
-    library.push(book);
-
-
+books.prototype.readToggle = function(){
+    this.read = !this.read
 }
 
 
-addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
-addBookToLibrary('Good boy','Thomas bid', 100, false)
-addBookToLibrary('Troy','brock lisner', 120, true)
-addBookToLibrary('bani adm', 'basem yousef', 180, true);
 
+
+
+function addBookToLibrary(title, author, pages, read){
+    let book = new books(crypto.randomUUID(), title, author, pages, read)
+
+    library.push(book)
+}
+
+
+addBookToLibrary('troy', 'hecktor', 10, true)
+addtoDisplay(library[library.length -1])
+addBookToLibrary('360 vacation days', 'Ezz', 5, false)
+addtoDisplay(library[library.length -1])
 
 
 
 function addtoDisplay(book){
 
-    let block = document.createElement('div');
-    block.setAttribute('style', 'margin: 10px;  padding: 10px;  border: 3px solid white;  border-radius: 20px;')
-    let headline = document.createElement('p')
-    headline.setAttribute('style', 'font-size: 20px;  font-weight: bold;  margin: 5px;')
-    headline.textContent = book.title
-    let display_btn = document.createElement('button')
-    display_btn.setAttribute('style', 'background-color: lightgreen; font-size: 16px; color: white;  font-weight: bold;  cursor: pointer;')
-    display_btn.dataset.action = 'display'
-    display_btn.textContent = 'Hide'
-    let read_btn = document.createElement('button')
-    read_btn.setAttribute('style', 'background-color: lightgreen; font-size: 16px; color: white;  font-weight: bold;  cursor: pointer; margin: 3px;')
-    read_btn.dataset.action = 'read'
-    read_btn.textContent = 'Read'
     
 
-    // unOrdered list to include all data expect headline ( book title )
-    let book_data = document.createElement('ul')
-    let book_author = document.createElement('li')
-    book_author.textContent = book.author
-    let book_pages = document.createElement('li')
-    book_pages.textContent = book.pages
-    let book_read = document.createElement('li')
-    book_read.textContent = book.read
-    book_data.appendChild(book_author)
-    book_data.appendChild(book_pages)
-    book_data.appendChild(book_read)
+        let card = document.createElement('div') 
+        card.classList.add('card')
+        let headline = document.createElement('p')
+        headline.textContent = book.title
+        headline.setAttribute('style', 'padding: 3px; margin: 5px;')
+        let list = document.createElement('ul')
+        let autor_element = document.createElement('li')
+        autor_element.textContent = book.author
+        let pages_element = document.createElement('li')
+        pages_element.textContent = book.pages
+        let read_element = document.createElement('li')
+        read_element.setAttribute('class','li_read')
+        if(book.read === 'on'){
+            read_element.textContent = 'had read'
+        }else read_element.textContent = 'Not Yet'
+
+        // form the dom
+        list.appendChild(autor_element)
+        list.appendChild(pages_element)
+        list.appendChild(read_element)
+
+        card.appendChild(headline)
+        card.appendChild(list)
 
 
+        
+        let btn = document.createElement("btn")
+        btn.setAttribute('style', 'background-color: yellow; font-size: 15px; padding: 3px; border-radius: 8px; cursor: pointer; margin: 5px;')
+        btn.dataset.id = book.id
+        btn.dataset.flag = 'flag'
+        btn.textContent = 'Remove'
+
+
+        let read_btn = document.createElement("btn")
+        read_btn.setAttribute('style', 'background-color: lightgreen; font-size: 15px; padding: 3px; border-radius: 8px; cursor: pointer; margin: 5px;')
+        read_btn.classList.add('read')
+        read_btn.dataset.id = book.id
+        if(book.id === true){
+            read_btn.textContent = 'Read'
+            read_btn.style.backgroundColor = 'lightgreen'
+        }else{
+            read_btn.textContent = 'Un Read'
+            read_btn.style.backgroundColor = 'red'
+        }
+        
+
+        //position of buttons
+        card.appendChild(btn)
+        card.appendChild(read_btn)
+
+
+        div.appendChild(card)
     
-
-    // final to put elements in a block
-    block.appendChild(headline)
-    block.appendChild(book_data)
-    block.appendChild(display_btn)
-    block.appendChild(read_btn)
-
-
-    // final to put block in a div
-    div.appendChild(block)
-    
-
 }
 
 
-library.forEach((book) => {
-
-    addtoDisplay(book)
-
-})
 
 
-
-btn.addEventListener("click", (e) => {
-
-    dialog.showModal()
-    
-        
-
+new_btn.addEventListener('click' ,(e) => {
+    if(e.target.id === 'btn'){
+        dialog.showModal()
+    }
 })
 
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    addBookToLibrary(inp[0].value,inp[1].value,inp[2].value, inp[3].value)
-    addtoDisplay(library[library.length - 1])
-    dialog.close()
+
+    e.preventDefault()
+    addBookToLibrary(
+        document.getElementsByClassName('inp')[0].value,
+        document.getElementsByClassName('inp')[1].value,
+        document.getElementsByClassName('inp')[2].value,
+        document.getElementsByClassName('inp')[3].value)
+    form.reset()
+    dialog.close()    
+    addtoDisplay(library[library.length -1 ])
+
 })
 
 
-div.addEventListener('click', e => {
-    if(e.target.dataset.action === 'display'){
-        e.target.parentElement.style.display = 'none'
+div.addEventListener('click', (e) => {
+    if(e.target.dataset.flag){
+        let ind = library.findIndex((book) => book.id === e.target.dataset.id )
+        library.splice(ind,1)
+        console.log(library);
+        e.target.parentElement.style.display='none';
     }
 })
 
-div.addEventListener('click', e => {
-    if(e.target.dataset.action === 'read'){
-        
-        if(e.target.textContent === 'Read'){
-            e.target.textContent = 'UnRead'
-            e.target.style.backgroundColor='red'
+
+div.addEventListener('click', (e) => {
+    if(e.target.classList.contains('read')){
+        let obj = library.find((book) => book.id === e.target.dataset.id)
+        let ind = library.findIndex((book) => book.id === e.target.dataset.id)
+        obj.readToggle()
+        let target = e.target.closest('.card').querySelector('.li_read')
+        if(obj.read === true){
+            target.textContent = 'had read'
+            e.target.textContent = 'Un Read'
+            e.target.style.backgroundColor = 'red'
         }else{
-            e.target.textContent = 'Read'
-            e.target.style.backgroundColor='lightgreen'
+             target.textContent = 'Not Yet'
+             e.target.textContent = 'Read'
+             e.target.style.backgroundColor = 'lightgreen'
+        }     
+        console.log(obj);
 
-        }
     }
 })
+
 
